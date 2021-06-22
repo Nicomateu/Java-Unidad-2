86b016b83f8b83a03a331b599a3c87f8c10c63bf
Nicomateu <85905231+Nicomateu@users.noreply.github.com>
2021-06-22 19:57:12 -0300

    Update Codo_a_Codo.java

--- src/Actividad_1/Codo_a_Codo.java
+++ src/Actividad_1/Codo_a_Codo.java
@@ -12,21 +12,31 @@ public class Codo_a_Codo {
             Persona p;
             
             System.out.println("Ingrese sus datos.");
+            
             System.out.print("Nombre: ");
             String nom = in.nextLine();
+            
             System.out.print("Apellido: ");
             String ape = in.nextLine();
+            
             System.out.print("Edad: ");
             int edad = in.nextInt();
             in.nextLine();
+            
             System.out.print("Hobbie: ");
             String hobbie = in.nextLine();
+            
             System.out.print("Editor de Codigo: ");
             String editor = in.nextLine();
+            
             System.out.print("Sistema Operativo: ");
             String so = in.nextLine(); 
+            
             System.out.println("\n\n\n\n");
+            
+            
             p = new Persona(nom, ape, hobbie, editor, so, edad);
+            
             System.out.println("Bienvenido al Sistema");
             System.out.println("");
             System.out.println("\u001B[42m"+"\u001B[37m"+"Los datos ingresados son:  "+"\u001B[0m");
