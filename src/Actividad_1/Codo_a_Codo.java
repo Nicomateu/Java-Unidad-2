bb1ef918ea8dc5054bd784606946afed55f2b421
nicolas <nicolas.mateu24@gmail.com>
2021-06-22 19:33:54 -0300

    Actividad Obligatoria de Java - Unidad 2

---
+++ src/Actividad_1/Codo_a_Codo.java
@@ -0,0 +1,37 @@
+
+package Actividad_1;
+
+import java.util.Scanner;
+import Actividad_1.Persona;
+
+
+public class Codo_a_Codo {
+
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            Persona p;
+            
+            System.out.println("Ingrese sus datos.");
+            System.out.print("Nombre: ");
+            String nom = in.nextLine();
+            System.out.print("Apellido: ");
+            String ape = in.nextLine();
+            System.out.print("Edad: ");
+            int edad = in.nextInt();
+            in.nextLine();
+            System.out.print("Hobbie: ");
+            String hobbie = in.nextLine();
+            System.out.print("Editor de Codigo: ");
+            String editor = in.nextLine();
+            System.out.print("Sistema Operativo: ");
+            String so = in.nextLine(); 
+            System.out.println("\n\n\n\n");
+            p = new Persona(nom, ape, hobbie, editor, so, edad);
+            System.out.println("Bienvenido al Sistema");
+            System.out.println("");
+            System.out.println("\u001B[42m"+"\u001B[37m"+"Los datos ingresados son:  "+"\u001B[0m");
+            System.out.println(p.toString()+"\u001B[0m");
+        }
+    }
+    
+}
---
+++ src/Actividad_1/Persona.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Actividad_1;
+
+/**
+ *
+ * @author Mateu
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String hobbie;
+    private String editor_codigo;
+    private String sistema_operativo;
+    private int edad;
+
+    public Persona(String nombre, String apellido, String hobbie, String editor_codigo, String sistema_operativo, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.hobbie = hobbie;
+        this.editor_codigo = editor_codigo;
+        this.sistema_operativo = sistema_operativo;
+        this.edad = edad;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getHobbie() {
+        return hobbie;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getEditor_codigo() {
+        return editor_codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSistema_operativo() {
+        return sistema_operativo;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setEditor_codigo(String editor_codigo) {
+        this.editor_codigo = editor_codigo;
+    }
+
+    public void setHobbie(String hobbie) {
+        this.hobbie = hobbie;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSistema_operativo(String sistema_operativo) {
+        this.sistema_operativo = sistema_operativo;
+    }
+
+    public String toString() {
+        return "\u001B[31m"+"Nombre: "+this.getNombre()+"\n"+"\u001B[31m"+"Apellido: "+this.getApellido()+"\n"+"\u001B[31m"+"Edad: "+this.getEdad()+"\n"+"\u001B[31m"+"Hobbie: "+this.getHobbie()+"\n"+"\u001B[31m"+"Editor de Codigo: "+this.getEditor_codigo()+"\n"+"\u001B[31m"+"Sistema Operativo: "+this.getSistema_operativo()+"\n";
+    }
+    
+    
+    
+    
+            
+    
+    
+    
+    
+}
